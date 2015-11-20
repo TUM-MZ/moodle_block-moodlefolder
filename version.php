1193c8261c2a1b0e279d3b87b05635f5acda6ce4
Dimitri Vorona <vorona@in.tum.de>
2015-11-20 16:28:36 +0100

    Added new required field for 3.0

--- version.php
+++ version.php
@@ -1,3 +1,4 @@
 <?php
 $plugin->version = 2015111900;  // YYYYMMDDHH (year, month, day, 24-hr time)
 $plugin->requires = 2010112400; // YYYYMMDDHH (This is the release version for Moodle 2.0)
+$plugin->component = 'block_moodlefolder';
